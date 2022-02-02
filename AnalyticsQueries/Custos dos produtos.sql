26808b69858a524e0e76f04a9011c2d129d8f3ab
estefanybueno123 <97976488+estefanybueno123@users.noreply.github.com>
2022-02-02 08:46:33 -0300

    Create Custos dos produtos.sql
    Verificando custo dos produtos, se houve aumento ou redução. Incluindo Fleg nos produtos em que teve aumento

---
+++ AnalyticsQueries/Custos dos produtos.sql
@@ -0,0 +1,17 @@
+/****** Verificando custo dos produtos, se houve aumento ou redução. Incluindo Fleg nos produtos em que teve aumento ******/
+SELECT [ProductID]
+      ,[StartDate]
+      ,[EndDate]
+      ,[StandardCost]
+	  ,LAG(StandardCost, 1, 0) Over (Partition by productID order by [startdate]) AS CustotAnterior
+	  ,CASE
+	  WHEN LAG(StandardCost, 1, 0) Over (Partition by productID order by [startdate]) = 0 THEN 0
+	  WHEN StandardCost > LAG(StandardCost, 1, 0) Over (Partition by productID order by [startdate]) THEN 1
+	  ELSE 0
+	  END AS FlgAumento -- Verificando se houve aumento no custo do produto
+	  ,CASE 
+		WHEN LAG(StandardCost, 1, 0) Over (Partition by productID order by [startdate]) > 0 THEN
+	 (StandardCost - LAG(StandardCost, 1, 0) Over (Partition by productID order by [startdate])) / LAG(StandardCost, 1, 0) Over (Partition by productID order by [startdate]) *100
+	 ELSE 0
+	 END AS PorcentagemAumento -- Verificando quantos % o custo aumentou ou diminuiu
+  FROM [AdventureWorks2019].[Production].[ProductCostHistory]
